f2dcd1fcf7e2f2d50fc934dcd22d57a20001e6a6
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2024-12-17 19:28:12 -0600

    Added some javascript for button functionality.

--- script.js
+++ script.js
@@ -22,3 +22,49 @@ const HarryPotterPartTwo = new Book(
 );
 
 const atomicHabbits = new Book("Atomic Habits", "James Clear", 320, false);
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 320, false);
+
+myLibrary.push(
+  HarryPotterPartOne,
+  HarryPotterPartTwo,
+  atomicHabbits,
+  theHobbit
+);
+
+const bookList = document.getElementById("book-list");
+
+function displayBooks() {
+  bookList.innerHTML = "";
+  myLibrary.forEach((book, index) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("card");
+
+    bookDiv.innerHTML = `
+    <h2 class="book-title">${book.title}</h2>
+    <div><p class="author">Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    </div>
+    <div class="card-buttons">
+    <button onclick="toggleRead(${index}, this)">${
+      book.isRead ? "Unread" : "Read"
+    }</button>
+    <button onclick="deleteBook(${index})">Delete</button>`;
+
+    bookList.appendChild(bookDiv);
+  });
+}
+
+function toggleRead(index, button) {
+  const book = myLibrary[index];
+  book.isRead = !book.isRead;
+
+  button.textContent = book.isRead ? "Unread" : "Read";
+}
+
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+
+displayBooks();
